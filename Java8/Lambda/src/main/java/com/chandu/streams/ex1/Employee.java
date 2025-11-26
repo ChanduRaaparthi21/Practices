c3b867185933d0f46474d10aecb5e5d6947d2f95
Chandu Raparthi <116146006+ChanduRaaparthi21@users.noreply.github.com>
2025-11-26 09:13:14 +0530

    streams practiced

---
+++ Java8/Lambda/src/main/java/com/chandu/streams/ex1/Employee.java
@@ -0,0 +1,92 @@
+package com.chandu.streams.ex1;
+
+public class Employee {
+
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private double salary;
+    private int age;
+    private String department;
+    private String jobTitle;
+
+    public Employee(int id, String firstName, String lastName, double salary, int age, String department, String jobTitle) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.age = age;
+        this.department = department;
+        this.jobTitle = jobTitle;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                ", age=" + age +
+                ", department='" + department + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+}
---
+++ Java8/Lambda/src/main/java/com/chandu/streams/ex1/StreamsEx1.java
@@ -0,0 +1,112 @@
+package com.chandu.streams.ex1;
+
+import java.util.List;
+
+public class StreamsEx1 {
+
+    public static void main(String[] args) {
+
+        List<Employee> employees = EmployeeData.get();
+
+
+
+        // Print all employees
+       /* employees.stream()
+                .forEach(e-> System.out.println(e));*/
+
+
+
+
+        // Print employees with salary greater than 20000 and last name starting with 'D'
+        /*employees.stream()
+                .filter(e->e.getSalary()>20000.0)
+                .filter(e->e.getLastName().startsWith("D"))
+                .forEach(e-> System.out.println(e));*/
+
+
+// Print employees sorted by first name
+           /* employees.stream()
+                    .sorted((e1,e2)->e1.getFirstName().compareTo(e2.getFirstName()))
+                    .forEach(e-> System.out.println(e));*/
+
+
+        // Print first names of employees sorted alphabetically
+        /*employees.stream()
+                .map(e->e.getFirstName())
+                .sorted()
+                .forEach(e-> System.out.println(e));*/
+
+
+
+        // Print employees from IT department
+        /*employees.stream()
+                .filter(e->e.getDepartment().equals("IT"))
+                .forEach(e-> System.out.println(e));*/
+
+
+
+
+        // Count number of employees in IT department
+        /*System.out.println(
+        employees.stream()
+                .filter(e->e.getDepartment().equals("IT"))
+                .count());*/
+
+
+        // Calculate total salary of employees in IT department
+       /* System.out.println(employees.stream()
+                        .filter(e->e.getDepartment().equals("IT"))
+                .mapToDouble(e -> e.getSalary())
+                .sum());*/
+
+
+
+        // Find the maximum salary among all employees
+        /*System.out.println(
+        employees.stream()
+                .mapToDouble(e->e.getSalary())
+                .max());
+*/
+
+
+        // Get a list of distinct first names of employees
+     /*   List<String> empNames = employees.stream()
+                .map(e -> e.getFirstName())
+                .distinct()
+                .toList();
+        System.out.println(empNames);*/
+
+
+      /*  List<String> empNames = employees.stream()
+                .map(e -> e.getFirstName())
+                .distinct()
+                .collect(Collectors.toList());
+        System.out.println(empNames);*/
+
+
+
+        // Get an unmodifiable list of distinct first names of employees
+       /* List<String> empNames = employees.stream()
+                .map(e -> e.getFirstName())
+                .distinct()
+                .collect(Collectors.toUnmodifiableList());
+//        empNames.add("chandu");
+        System.out.println(empNames);*/
+
+
+
+        // Create a map of employee first names to their salaries
+
+        /*Map<String, String> empToMap = employees.stream()
+                .collect(Collectors.toMap(
+                        e -> e.getFirstName(),
+                        e -> String.valueOf(e.getSalary()),
+                        (a,b)-> a+" , "+ b
+                ))
+                ;
+        System.out.println(empToMap);*/
+
+
+
+    }
+}
